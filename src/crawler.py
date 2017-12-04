0c5c167adeeec90b4f38d8da33b6581c46157cba
Scott McLane Gardner <sgardne@uark.edu>
2017-12-04 16:12:32 -0600

    Got network parsing and checking done. Need to create database now.

--- src/crawler.py
+++ src/crawler.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import socket
+import netaddr
 
 '''
 Given a list of networks or IP's find any network devices with open SSH or Telnet ports.
@@ -6,23 +8,23 @@ Do additional scan to find out if it's actually a network switch.
 Populate a database with IP addresses (perhaps other metadata) of extant switches.
 '''
 
-def get_list():
-    try:
-        with open('networks.csv') as networks:
-            _network_list = networks.read()
-            return _network_list
-    except Exception as e:
-        pass
-
-def check_for_ssh(network):
+def check_for_ssh(network_list):
     # Work through the range testing for tcp/22 access
     # Return a list of IP's with access or "none".
-    pass
+    for line in network_list:
+        # Need to break up networks and try each node.
+        _network = netaddr.IPNetwork(line.rstrip())
+        for address in _network.iter_hosts():
+            try:
+                print("Trying " + str(address))
+                sock = socket.create_connection((str(address), '22'), timeout=3)
+            except socket.timeout as timeout:
+                print("Connection to " + str(address) + " timed out.")
 
 def main():
-    network_list = get_list()
+    network_list = open('networks.csv', 'r')
     # Each line is a network in CIDR format or a single IP address.
-    ssh_ips = check_for_ssh(network)
+    ssh_ips = check_for_ssh(network_list)
 
 if __name__ == "__main__":
     main()
